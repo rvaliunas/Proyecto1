70e8d38164ea1df3f8af0a694a4f07a1227d1875
alumnoFI <alumnoFI@facultades.ort.edu.uy>
2023-03-15 20:14:47 -0300

    Parte en clase

---
+++ Primer proyecto/Alumno.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primer_proyecto
+{
+    internal class Alumno
+    {
+        private int numero;
+        private string nombres;
+        private string apellidos;
+        private DateTime fechaNacimiento;
+        
+        //properties en C#
+
+    public int Numero
+        {
+            get { return numero; }
+            set { numero = value; }
+        }
+
+        public int GetNumero()
+        {
+            return this.numero;
+        }
+        
+        public void SetNumero(int n)
+        {
+            this.numero = n;
+        }
+    }
+}
---
+++ Primer proyecto/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primer_proyecto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Alumno unAlumno = new Alumno();
+            unAlumno.Numero = 123456;
+
+            Console.WriteLine(
+                string.Format("Se creó el alumno con numero {0}", unAlumno.Numero)
+                );
+
+            //si no ponemos esta lìnea, la consola se abre y cierra muy rápido.
+            Console.ReadLine();
+        }
+    }
+}
